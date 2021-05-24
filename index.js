3503bc78728fda07f98220fb9e371639897d293b
TreciaKS <tksekgalo@gmail.com>
2021-05-24 23:27:47 +0200

    Updated index.js, added copy()

---
+++ index.js
@@ -0,0 +1,13 @@
+// Enable copy btn
+function copy() {
+  document.getElementById("copy").select();
+  document.execCommand("copy");
+  alert("Copied To Clipboard!");
+} 
+
+// Prints source code on textarea 
+function sourceCode () {
+  outputCode.value = `border-radius: ${top.value} ${right.value} ${bottom.value} ${left.value};`
+
+  
+}
